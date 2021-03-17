8a616d1793602da79280761fb2194fe72bfe1448
Marcin Bodziak <mbodziak70@gmail.com>
2021-03-18 00:05:11 +0100

    inicjalny commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+#*********************************************************************
+#* read_item - /items/{item_id}
+#*********************************************************************
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+#*********************************************************************
+#* root - /
+#*********************************************************************
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
